76e23ff81a4526cb6b2f21399dcfabe169e4806b
ZALCK <charbelzalk@gmail.com>
2021-03-14 06:50:37 +0000

    Datasource Configuration

--- timeManagement/src/main/java/com/esmt/timeManagement/TimeManagementApplication.java
+++ timeManagement/src/main/java/com/esmt/timeManagement/TimeManagementApplication.java
@@ -2,8 +2,11 @@ package com.esmt.timeManagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
+@PropertySources({ @PropertySource("classpath:db.properties") })
 public class TimeManagementApplication {
 
 	public static void main(String[] args) {
